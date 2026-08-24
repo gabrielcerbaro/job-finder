4cd8fd4571ab677dc162fc278999a1ddcd8aae8c
Gabriel Cerbaro <gabrielis2one@yahoo.com>
2026-08-24 20:03:15 -0300

    feat: add job routes and HTML structure with Bootstrap styling

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
-const express = require('express');
-const app = express();
-const port = 3000;
-const db = require('./db/connections')
+const express    = require('express');
+const app        = express();
+const port       = 3000;
+const db         = require('./db/connections')
+const bodyParser = require('body-parser');
 
 app.listen(port, function () {
     console.log(`O express está ouvindo a porta ${port}`)
 });
+
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // db connection
 db
@@ -20,3 +24,6 @@ db
 app.get('/', (req, res) => {
     res.send('Está funcionando 3');
 });
+
+// jobs routes
+app.use('/jobs', require('./routes/jobs'))
